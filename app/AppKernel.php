4897ef65498cc43342cff733f6915e18aa508a50
Bogdan Smerechinskiy <smerechinskiy@gmail.com>
2017-07-25 00:45:38 +0300

    generate my ApiBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle(),
             new FOS\RestBundle\FOSRestBundle(),
+            new ApiBundle\ApiBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
